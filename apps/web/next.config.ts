d4c101ad0f1de05321827deab3f69efc4b72ed9c
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-12-02 04:10:18 +0330

    🔧 build(dotenv): load env variales
    ensured accessablity of environment vaiables, configured dotenv  within next configuration.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@repo/ui"],
+  env: {
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+    WEBHOOKS_SECRET: process.env.WEBHOOKS_SECRET,
+  },
 };
 
 export default nextConfig;
