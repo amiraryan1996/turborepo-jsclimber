5f719ca4665828aab6d21132849ab92ef3005ba6
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-12-02 03:49:30 +0330

    ci:test

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Welcome to Documents</h1>
+      <h1 className="mb-4 text-2xl font-bold">
+        Welcome to Jsclimber's Document
+      </h1>
       <p className="text-fd-muted-foreground">
         You can open{" "}
         <Link
